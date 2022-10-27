b428932579273c4579f923e64208e71881eb3fda
chienaeae <chienaeae@gmail.com>
2022-10-27 14:55:29 +0800

    feat: migration added card_linked_generic_logic table

--- mochi/migrations/20221017135708_init-card.up.sql
+++ mochi/migrations/20221017135708_init-card.up.sql
@@ -5,4 +5,13 @@ CREATE TABLE IF NOT EXISTS card (
   description TEXT NOT NULL,
   creation_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
   PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS card_linked_generic_logic(
+  id UUID NOT NULL,
+  generic_logic_id UUID NOT NULL,
+  PRIMARY KEY (id, generic_logic_id),
+  FOREIGN KEY (id) REFERENCES card(id) ON
+  UPDATE
+    RESTRICT ON DELETE CASCADE
+);
